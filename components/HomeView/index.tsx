08cb7477fffeb3e52be210fbd8e238a8b09fe458
BarLis <barlis@BarLiss-MacBook-Pro.local>
2022-12-06 16:44:40 +0200

    all wallet providers in one component

--- components/HomeView/index.tsx
+++ components/HomeView/index.tsx
@@ -1,11 +1,7 @@
 import { FC } from 'react';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import styles from '../../styles/Home.module.css';
-import { ConnectionProvider } from '@solana/wallet-adapter-react';
 import dynamic from 'next/dynamic';
-import { clusterApiUrl } from '@solana/web3.js';
-import { useMemo, useState } from 'react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 
 const WalletProvider = dynamic(
   () => import('../../context/SolanaContext/ClientWalletProvider'),
@@ -15,30 +11,22 @@ const WalletProvider = dynamic(
 );
 
 export const HomeView: FC = () => {
-  const [network] = useState(WalletAdapterNetwork.Devnet);
-  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-
   return (
     <>
-      <ConnectionProvider endpoint={endpoint}>
-        <WalletProvider>
-          <h1>KaChing Cash Register Admin Panel</h1>
-          <section id="dashboard">
-            <header>
-              {'header'}
-              <div className={styles.alignRight}>
-                <WalletMultiButton />
-              </div>
-            </header>
-            <nav>{'sidebar navigation'}</nav>
-            <main>
-              {'main content'}
-              <p></p>
-            </main>
-          </section>
-          <footer className={styles.footer}>Powered by UnCaged Studios</footer>
-        </WalletProvider>
-      </ConnectionProvider>
+      <WalletProvider>
+        <h1>KaChing Cash Register Admin Panel</h1>
+        <section id="dashboard">
+          <header>
+            {'header'}
+            <div className={styles.alignRight}>
+              <WalletMultiButton />
+            </div>
+          </header>
+          <nav>{'sidebar navigation'}</nav>
+          <main>{'main content'}</main>
+        </section>
+        <footer className={styles.footer}>Powered by UnCaged Studios</footer>
+      </WalletProvider>
     </>
   );
 };
--- context/SolanaContext/ClientWalletProvider.tsx
+++ context/SolanaContext/ClientWalletProvider.tsx
@@ -1,20 +1,28 @@
 import { WalletProviderProps } from '@solana/wallet-adapter-react';
 import { WalletProvider } from '@solana/wallet-adapter-react';
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 
 import('@solana/wallet-adapter-react-ui/styles.css' as any);
 
+import { clusterApiUrl } from '@solana/web3.js';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { ConnectionProvider } from '@solana/wallet-adapter-react';
+
 export function ClientWalletProvider(
   props: Omit<WalletProviderProps, 'wallets'>
 ): JSX.Element {
+  const [network] = useState(WalletAdapterNetwork.Devnet);
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
   const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
 
   return (
-    <WalletProvider wallets={wallets} {...props}>
-      <WalletModalProvider {...props} />
-    </WalletProvider>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} {...props}>
+        <WalletModalProvider {...props} />
+      </WalletProvider>
+    </ConnectionProvider>
   );
 }
 
