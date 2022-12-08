79e4981a91a67eda3e220167ebbada960a2d89c2
BarLis <barlis@BarLiss-MacBook-Pro.local>
2022-12-08 13:48:53 +0200

    fix key in table

--- components/ka-ching/cash-register/List.tsx
+++ components/ka-ching/cash-register/List.tsx
@@ -13,6 +13,7 @@ export const List: FC = () => {
 
   const getAllKaChingCashRegisters = async () => {
     const kaChingCashRegisters = await fetchAllKaChingCashRegisters(connection);
+    console.log(kaChingCashRegisters);
     setKaChindData(kaChingCashRegisters);
   };
 
@@ -32,34 +33,15 @@ export const List: FC = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            {kaChingData &&
-              kaChingData.map((val, idx) => (
-                <>
-                  <td key={idx} className="border border-slate-700">
-                    {val.address.toString()}
-                  </td>
-                  <td key={idx}>{val.cashRegisterId}</td>
-                  <td key={idx}>{val.cashierPublicKey.toString()}</td>
-                  <td key={idx}>{val.orderSignersWhitelist.toString()}</td>
-                </>
-              ))}
-          </tr>
-          <tr>
-            <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-            <td>Malcolm Lockyer</td>
-            <td>1961</td>
-          </tr>
-          <tr>
-            <td>Witchy Woman</td>
-            <td>The Eagles</td>
-            <td>1972</td>
-          </tr>
-          <tr>
-            <td>Shining Star</td>
-            <td>Earth, Wind, and Fire</td>
-            <td>1975</td>
-          </tr>
+          {kaChingData &&
+            kaChingData.map((val, idx) => (
+              <tr key={idx}>
+                <td>{val.address.toString()}</td>
+                <td>{val.cashRegisterId}</td>
+                <td>{val.cashierPublicKey.toString()}</td>
+                <td>{val.orderSignersWhitelist.toString()}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </>
