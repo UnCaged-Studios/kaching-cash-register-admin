356cb349c3add46ab266687f5c18730bde02ea1d
BarLisML <119160534+BarLisML@users.noreply.github.com>
2022-12-08 09:19:16 +0200

    initial react-wallet-adapter integration (#2)
    Co-authored-by: BarLis <barlis@BarLiss-MacBook-Pro.local>
    Co-authored-by: Tom Peres <perestom@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,15 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { HomeView } from '../routing/index';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>KaChing Cash Register Admin Panel</title>
         <meta name="description" content="" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <h1>KaChing Cash Register Admin Panel</h1>
-
-      <section id="dashboard">
-        <nav>{'sidebar navigation'}</nav>
-        <header>{'header'}</header>
-        <main>{'main content'}</main>
-      </section>
-
-      <footer className={styles.footer}>Powered by UnCaged Studios</footer>
+      <HomeView />
     </div>
   );
 }
