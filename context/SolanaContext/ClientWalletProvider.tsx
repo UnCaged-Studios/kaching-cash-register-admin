d39adb1199931472152184735b4704d59ab35ff3
BarLis <barlis@BarLiss-MacBook-Pro.local>
2022-12-14 12:23:15 +0200

    clean code

--- context/SolanaContext/ClientWalletProvider.tsx
+++ context/SolanaContext/ClientWalletProvider.tsx
@@ -1,12 +1,9 @@
 import { WalletProviderProps } from '@solana/wallet-adapter-react';
 import { WalletProvider } from '@solana/wallet-adapter-react';
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
-import { useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { ConnectionProvider } from '@solana/wallet-adapter-react';
-import { useForm } from 'react-hook-form';
 import('@solana/wallet-adapter-react-ui/styles.css' as any);
-import styles from '../../styles/Home.module.css';
 
 export function ClientWalletProvider(
   props: Omit<WalletProviderProps, 'wallets'>
