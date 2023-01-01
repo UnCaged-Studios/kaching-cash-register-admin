14c8f019e720026f0e0a7d75e0e464d201efc3ec
BarLis <barlis@BarLiss-MacBook-Pro.local>
2023-01-01 15:06:09 +0200

    Option to filter the list by cashier in CR list

--- components/ka-ching/cash-register/CashRegisterList.tsx
+++ components/ka-ching/cash-register/CashRegisterList.tsx
@@ -16,13 +16,15 @@ import {
   Paper,
   Button,
 } from '@mui/material';
+import { useWallet } from '@solana/wallet-adapter-react';
 
 export const CashRegisterList: FC = () => {
   const [kaChingData, setKaChindData] = useState<KachingCashRegisterModel[]>(
     []
   );
+  const cashier = useWallet();
 
-  const getAllKaChingCashRegisters = async () => {
+  const getAllKaChingCashRegisters = async (filter: boolean) => {
     try {
       const endpoint = getLocalStorage('endpoint');
       if (endpoint) {
@@ -30,7 +32,30 @@ export const CashRegisterList: FC = () => {
         const kaChingCashRegisters = await fetchAllKaChingCashRegisters(
           connection
         );
-        setKaChindData(kaChingCashRegisters);
+
+        if (filter) {
+          const filterKaChingCashRegisters = kaChingCashRegisters.reduce(
+            (filtered, val) => {
+              if (
+                val.cashierPublicKey.toString() ===
+                cashier.publicKey?.toString()
+              ) {
+                const newValue = {
+                  address: val.address,
+                  cashRegisterId: val.cashRegisterId,
+                  cashierPublicKey: val.cashierPublicKey,
+                  orderSignersWhitelist: val.orderSignersWhitelist,
+                };
+                filtered.push(newValue);
+              }
+              return filtered;
+            },
+            []
+          );
+          setKaChindData(filterKaChingCashRegisters);
+        } else {
+          setKaChindData(kaChingCashRegisters);
+        }
       }
     } catch (error) {
       console.log(JSON.stringify(error));
@@ -40,62 +65,81 @@ export const CashRegisterList: FC = () => {
   const [displayForm, setDisplayForm] = useState(false);
   return (
     <div>
-      <Button
-        sx={{ m: 1 }}
-        variant="contained"
-        onClick={() => {
-          setDisplayForm(true);
-          getAllKaChingCashRegisters();
-        }}
-      >
-        Create List
-      </Button>
-      {displayForm && (
-        <div style={{ margin: '0.5rem' }}>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  {Object.keys(Object.assign({}, ...kaChingData)).map(
-                    (val, id) => (
-                      <TableCell
-                        key={id}
+      {cashier.connected ? (
+        <>
+          <Button
+            sx={{ m: 1 }}
+            variant="contained"
+            onClick={() => {
+              setDisplayForm(true);
+              getAllKaChingCashRegisters(false);
+            }}
+          >
+            Fetch All CashRegisters
+          </Button>
+          <Button
+            sx={{ m: 1 }}
+            variant="contained"
+            onClick={() => {
+              setDisplayForm(true);
+              getAllKaChingCashRegisters(true);
+            }}
+          >
+            Fetch All CashRegisters By Cashier
+          </Button>
+          {displayForm && (
+            <div style={{ margin: '0.5rem' }}>
+              <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      {Object.keys(Object.assign({}, ...kaChingData)).map(
+                        (val, id) => (
+                          <TableCell
+                            key={id}
+                            sx={{
+                              maxWidth: 30,
+                              overflow: 'hidden',
+                              textOverflow: 'ellipsis',
+                            }}
+                          >
+                            {val.toString().charAt(0).toUpperCase() +
+                              val.slice(1)}
+                          </TableCell>
+                        )
+                      )}
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {kaChingData?.map((item, idx) => (
+                      <TableRow
+                        key={idx}
                         sx={{
-                          maxWidth: 30,
-                          overflow: 'hidden',
-                          textOverflow: 'ellipsis',
+                          '&:last-child td, &:last-child th': { border: 0 },
                         }}
                       >
-                        {val.toString().charAt(0).toUpperCase() + val.slice(1)}
-                      </TableCell>
-                    )
-                  )}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {kaChingData?.map((item, idx) => (
-                  <TableRow
-                    key={idx}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                  >
-                    {Object.values(item).map((val: any, id) => (
-                      <TableCell
-                        key={id}
-                        sx={{
-                          maxWidth: 30,
-                          overflow: 'hidden',
-                          textOverflow: 'ellipsis',
-                        }}
-                      >
-                        {val.toString()}
-                      </TableCell>
+                        {Object.values(item).map((val: any, id) => (
+                          <TableCell
+                            key={id}
+                            sx={{
+                              maxWidth: 30,
+                              overflow: 'hidden',
+                              textOverflow: 'ellipsis',
+                            }}
+                          >
+                            {val.toString()}
+                          </TableCell>
+                        ))}
+                      </TableRow>
                     ))}
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </div>
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </div>
+          )}
+        </>
+      ) : (
+        <h1>You are not connected to the wallet</h1>
       )}
     </div>
   );
