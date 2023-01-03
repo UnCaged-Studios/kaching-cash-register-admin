2c9d1f3e4c273d889ade1d5be106928339790355
BarLis <barlis@BarLiss-MacBook-Pro.local>
2023-01-03 13:35:55 +0200

    Clean code

--- components/ka-ching/cash-register/CashRegisterList.tsx
+++ components/ka-ching/cash-register/CashRegisterList.tsx
@@ -6,17 +6,9 @@ import {
 } from '../../../utils/sdk';
 import { Connection } from '@solana/web3.js';
 import { getLocalStorage } from '../../../utils/localStorageHandle';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-  Button,
-} from '@mui/material';
+import { Button } from '@mui/material';
 import { useWallet } from '@solana/wallet-adapter-react';
+import { List } from '../../List';
 
 export const CashRegisterList: FC = () => {
   const [kaChingData, setKaChindData] = useState<KachingCashRegisterModel[]>(
@@ -32,7 +24,6 @@ export const CashRegisterList: FC = () => {
         const kaChingCashRegisters = await fetchAllKaChingCashRegisters(
           connection
         );
-
         if (filter) {
           const filterKaChingCashRegisters: KachingCashRegisterModel[] =
             kaChingCashRegisters.reduce(
@@ -65,7 +56,6 @@ export const CashRegisterList: FC = () => {
       console.log(JSON.stringify(error));
     }
   };
-
   const [displayForm, setDisplayForm] = useState(false);
   return (
     <div>
@@ -91,56 +81,7 @@ export const CashRegisterList: FC = () => {
           >
             Fetch All CashRegisters By Cashier
           </Button>
-          {displayForm && (
-            <div style={{ margin: '0.5rem' }}>
-              <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                  <TableHead>
-                    <TableRow>
-                      {Object.keys(Object.assign({}, ...kaChingData)).map(
-                        (val, id) => (
-                          <TableCell
-                            key={id}
-                            sx={{
-                              maxWidth: 30,
-                              overflow: 'hidden',
-                              textOverflow: 'ellipsis',
-                            }}
-                          >
-                            {val.toString().charAt(0).toUpperCase() +
-                              val.slice(1)}
-                          </TableCell>
-                        )
-                      )}
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {kaChingData?.map((item, idx) => (
-                      <TableRow
-                        key={idx}
-                        sx={{
-                          '&:last-child td, &:last-child th': { border: 0 },
-                        }}
-                      >
-                        {Object.values(item).map((val: any, id) => (
-                          <TableCell
-                            key={id}
-                            sx={{
-                              maxWidth: 30,
-                              overflow: 'hidden',
-                              textOverflow: 'ellipsis',
-                            }}
-                          >
-                            {val.toString()}
-                          </TableCell>
-                        ))}
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-            </div>
-          )}
+          {displayForm && <List arr={kaChingData} />}
         </>
       ) : (
         <h1>You are not connected to the wallet</h1>
