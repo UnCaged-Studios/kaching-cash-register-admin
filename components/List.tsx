d8bf046937c88ecd7096874c67aa4cf1ee67f4c5
BarLis <barlis@BarLiss-MacBook-Pro.local>
2023-01-09 18:13:53 +0200

    Add TablePagination to list

--- components/List.tsx
+++ components/List.tsx
@@ -5,53 +5,83 @@ import {
   TableCell,
   TableContainer,
   TableHead,
+  TablePagination,
   TableRow,
 } from '@mui/material';
+import { ChangeEvent, useRef, useState } from 'react';
 
 export const List = (props: { arr: any[] }) => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+    console.log('handleChangeRowsPerPage: ', rowsPerPage);
+  };
   return (
-    <div style={{ margin: '0.5rem' }}>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+    <div style={{ margin: '0.5rem', marginBottom: '-1rem' }}>
+      <TableContainer component={Paper} ref={inputRef}>
+        <Table
+          sx={{ minWidth: 650 }}
+          stickyHeader
+          size="small"
+          aria-label="sticky table"
+        >
           <TableHead>
             <TableRow>
               <TableCell>#</TableCell>
               {Object.keys(Object.assign({}, ...props.arr)).map((val, id) => (
-                <TableCell key={id} align="center">
+                <TableCell key={id} align="center" ref={inputRef}>
                   {val.toString().charAt(0).toUpperCase() + val.slice(1)}
                 </TableCell>
               ))}
             </TableRow>
           </TableHead>
           <TableBody>
-            {props.arr?.map((item, idx) => (
-              <TableRow
-                key={idx}
-                sx={{
-                  '&:last-child td, &:last-child th': { border: 0 },
-                }}
-              >
-                <TableCell component="th" scope="row">
-                  {idx + 1}
-                </TableCell>
-                {Object.values(item).map((val: any, id) => (
-                  <TableCell
-                    key={id}
-                    sx={{
-                      maxWidth: 30,
-                      overflow: 'hidden',
-                      textOverflow: 'ellipsis',
-                    }}
-                    align="center"
-                  >
-                    {val?.toString()}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))}
+            {props.arr
+              ?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((item, idx) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={idx}>
+                    <TableCell component="th" scope="row" ref={inputRef}>
+                      {page * 10 + idx + 1}
+                    </TableCell>
+                    {Object.values(item).map((val: any, id) => {
+                      return (
+                        <TableCell
+                          key={id}
+                          sx={{
+                            maxWidth: 30,
+                            overflow: 'hidden',
+                            textOverflow: 'ellipsis',
+                          }}
+                          align="center"
+                        >
+                          {val?.toString()}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
           </TableBody>
         </Table>
       </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10]}
+        component="div"
+        count={props.arr.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
     </div>
   );
 };
