5f62895a83fa894e149df77edfd694151a3a58a0
BarLis <barlis@BarLiss-MacBook-Pro.local>
2023-01-31 10:30:46 +0200

    update and clean CashBoxContext component and cashBoxTxns

--- components/ka-ching/cash-register/CreateCashBox.tsx
+++ components/ka-ching/cash-register/CreateCashBox.tsx
@@ -1,70 +1,73 @@
-import { Box, Button, Input, TextField } from '@mui/material';
+import { Box, Button, TextField, Typography } from '@mui/material';
 import { useWallet } from '@solana/wallet-adapter-react';
-import { Connection, PublicKey } from '@solana/web3.js';
-import { createContext, useState } from 'react';
+import { PublicKey } from '@solana/web3.js';
+import { createContext, useEffect, useState } from 'react';
 import type { FC } from 'react';
-import { getLocalStorage } from '../../../utils/localStorageHandle';
 import { csvFileToArray } from '../../../utils/csvFileToArray';
 import { List } from '../../List';
 import { getAssociatedTokenAddressSync } from '@solana/spl-token';
 import { SignTransfersForm, Transfer } from '../../SignTransfersForm';
 import { cashBoxTxns } from '../../../utils/cashBoxTxns';
+import { CSVInput } from '../../CSVInput';
+import {
+  getLocalStorage,
+  setLocalStorage,
+} from '../../../utils/localStorageHandle';
 
 export type cashBoxModel = {
   kaChingToken: string;
   mintToken: string;
   amount: string;
   decimal: string;
+  status?: string;
 };
 
 export const CashBoxContext = createContext<any>(undefined);
 
 export const CreateCashBox: FC = () => {
   const cashier = useWallet();
-  const [file, setFile] = useState();
-  const [cashBoxArray, setCashBoxArray] = useState<cashBoxModel[]>([]);
-  const [allTransfers, setAllTransfers] = useState<cashBoxModel[]>([]);
-  const [transfers, setTransfers] = useState<Transfer[] | undefined>(undefined);
-  const [startFrom, setStartFrom] = useState<number | undefined>(undefined);
-  const [limit, setLimit] = useState<number | undefined>(undefined);
 
-  const handleOnChange = async (event: any) => {
-    try {
-      setFile(event.target.files[0]);
-    } catch (error) {
-      console.log(JSON.stringify(error));
-    }
+  const [file, setFile] = useState<File | undefined>(undefined);
+  const [showImportBtn, setShowImportBtn] = useState<boolean>(false);
+  const [txStatus, setTxStatus] = useState<string | undefined>(undefined);
+  const [allTransfers, setAllTransfers] = useState<cashBoxModel[] | undefined>(
+    undefined
+  );
+  const [transfers, setTransfers] = useState<Transfer[] | undefined>(undefined);
+  const [startFrom, setStartFrom] = useState<number>(0);
+  const [showSignTxBtn, setShowSignTxBtn] = useState<boolean>(false);
+
+  const resetValues = async () => {
+    setFile(undefined);
+    setShowImportBtn(false);
+    setTxStatus(undefined);
+    setAllTransfers(undefined);
+    setTransfers(undefined);
+    setStartFrom(0);
+    setShowSignTxBtn(false);
   };
 
   const handleOnSubmit = async (e: any) => {
     try {
-      e.preventDefault();
-      const fileReader = new FileReader();
-      if (file) {
-        fileReader.onload = async (event: any) => {
-          const text = event.target.result;
-          const array = await csvFileToArray(text);
-          if (array!.length > 0) {
-            setCashBoxArray(array!);
-          }
-        };
-        fileReader.readAsText(file);
-      }
-    } catch (error) {
-      console.log(JSON.stringify(error));
-    }
-  };
-
-  const createAndFetchCashBox = async () => {
-    try {
-      const endpoint = getLocalStorage('endpoint');
-      if (endpoint && cashBoxArray.length > 0) {
-        const connection = new Connection(JSON.parse(endpoint!));
-        const tx = await cashBoxTxns(cashBoxArray, cashier.publicKey!);
-        if (tx!.transactionList.instructions.length > 0) {
-          await cashier.sendTransaction(tx!.transactionList, connection);
+      const keys = Object.keys(localStorage);
+      const found = keys.find((fileName) => fileName === file!.name);
+      if (!found) {
+        e.preventDefault();
+        const fileReader = new FileReader();
+        if (file) {
+          fileReader.onload = async (event: any) => {
+            const csvArr = await csvFileToArray(event.target.result);
+            if (csvArr!.length > 0) {
+              const txArray = await cashBoxTxns(csvArr!, cashier);
+              setAllTransfers(txArray!);
+            }
+          };
+          fileReader.readAsText(file);
         }
-        setAllTransfers(tx!.txArray);
+      } else {
+        const lsTxt: string | null = getLocalStorage(file!.name);
+        const txArr: cashBoxModel[] | undefined = await csvFileToArray(lsTxt!);
+        setAllTransfers(txArr!);
       }
     } catch (error) {
       console.log(JSON.stringify(error));
@@ -92,19 +95,16 @@ export const CreateCashBox: FC = () => {
   };
 
   const createTransfers = async () => {
+    setShowSignTxBtn(true);
     try {
       if (
-        startFrom &&
+        allTransfers &&
         startFrom > 0 &&
-        startFrom <= limit! &&
-        limit! <= allTransfers.length &&
-        allTransfers
+        startFrom <= Math.ceil(allTransfers.length / 10)
       ) {
-        const startFromIdx = Number(startFrom) - 1;
+        const startFromIdx = (startFrom - 1) * 10;
         setTransfers(
-          allTransfers
-            ?.slice(startFromIdx, startFromIdx + limit!)
-            .map(toTransfer)
+          allTransfers?.slice(startFromIdx, startFromIdx + 10).map(toTransfer)
         );
         if (!transfers) throw new Error('missing transfers');
       }
@@ -113,19 +113,56 @@ export const CreateCashBox: FC = () => {
     }
   };
 
+  useEffect(() => {
+    const startIdx = (startFrom! - 1) * 10;
+    const endIdx = transfers?.length;
+    let status;
+    if (allTransfers && txStatus) {
+      setAllTransfers(
+        allTransfers.map((value, idx) => {
+          idx >= startIdx && idx < startIdx + endIdx!
+            ? (status = txStatus)
+            : (status = value.status);
+          const newValue: cashBoxModel = {
+            kaChingToken: value.kaChingToken,
+            mintToken: value.mintToken,
+            amount: value.amount,
+            decimal: value.decimal,
+            status,
+          };
+          return newValue;
+        })
+      );
+      if (file) {
+        const csvStr = [
+          ['kaChingToken', 'mintToken', 'amount', 'decimal', 'status'],
+          ...allTransfers.map((item) => [
+            item.kaChingToken,
+            item.mintToken,
+            item.amount,
+            item.decimal,
+            item.status,
+          ]),
+        ]
+          .map((e) => e.join(','))
+          .join('\n');
+        setLocalStorage(file.name, csvStr);
+      }
+      setTxStatus(undefined);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [txStatus]);
+
   return (
     <div>
       {cashier.connected ? (
         <div style={{ textAlign: 'center' }}>
-          <form>
-            <Input
-              sx={{ m: 1 }}
-              type={'file'}
-              id={'csvFileInput'}
-              onChange={handleOnChange}
-              inputProps={{ accept: '.csv' }}
-              size="small"
-            />
+          <CashBoxContext.Provider
+            value={{ setFile, setShowImportBtn, resetValues }}
+          >
+            <CSVInput />
+          </CashBoxContext.Provider>
+          {showImportBtn && (
             <Button
               sx={{ m: 1 }}
               variant="contained"
@@ -133,80 +170,56 @@ export const CreateCashBox: FC = () => {
                 handleOnSubmit(e);
               }}
             >
-              IMPORT CSV
-            </Button>
-          </form>
-          <br />
-          {cashBoxArray && (
-            <span>CSV contains {cashBoxArray.length} transfers</span>
-          )}
-          <br />
-          {cashBoxArray.length > 0 && (
-            <Button
-              sx={{ m: 1 }}
-              variant="contained"
-              onClick={() => {
-                createAndFetchCashBox();
-              }}
-            >
               Create & Fetch CashBox
             </Button>
           )}
-          {allTransfers.length > 0 && (
+          {allTransfers && allTransfers.length > 0 && (
             <>
               <List arr={allTransfers} />
+              <Typography color={'black'}>
+                {' '}
+                {startFrom > 0 &&
+                startFrom <= Math.ceil(allTransfers.length / 10)
+                  ? 'Batch No ' +
+                    startFrom +
+                    ' : from line ' +
+                    (startFrom * 10 - 9) +
+                    ' to line ' +
+                    (startFrom * 10 > allTransfers.length
+                      ? allTransfers.length
+                      : startFrom * 10)
+                  : ''}
+              </Typography>
+              <br />
               <TextField
-                sx={{ m: 1, marginTop: 3 }}
                 id="outlined-basic"
-                label="Start From"
+                label="Choose Batch"
                 variant="outlined"
                 value={startFrom}
-                onChange={(v) => setStartFrom(Number(v.target.value))}
+                onChange={(v) => {
+                  setShowSignTxBtn(false);
+                  setStartFrom(Number(v.target.value));
+                }}
                 size="small"
                 required
                 error={
                   startFrom === undefined ||
-                  startFrom > limit! ||
+                  startFrom > Math.ceil(allTransfers.length / 10) ||
                   startFrom <= 0
                 }
                 helperText={
-                  startFrom === undefined
-                    ? 'Must Required Start From'
-                    : startFrom > limit!
-                    ? 'The Start is greater than the limit'
-                    : startFrom <= 0
-                    ? 'The Start cannot be less than or equal to 0'
-                    : ' '
-                }
-                type={'number'}
-              />
-              <TextField
-                sx={{ m: 1, marginTop: 3 }}
-                id="outlined-basic"
-                label="Limit"
-                variant="outlined"
-                value={limit}
-                onChange={(v) => setLimit(Number(v.target.value))}
-                size="small"
-                required
-                error={
-                  limit === undefined ||
-                  limit > cashBoxArray.length ||
-                  limit < startFrom!
-                }
-                helperText={
-                  limit === undefined
-                    ? 'Must Required Limit'
-                    : limit! > cashBoxArray.length
-                    ? 'The limit is too big'
-                    : limit! < startFrom!
-                    ? 'The limit is lower than the Start From'
+                  startFrom === undefined || startFrom === 0
+                    ? 'Must Choose Batch'
+                    : startFrom > Math.ceil(allTransfers.length / 10)
+                    ? 'The Batch you selected does not exist'
+                    : startFrom < 0
+                    ? 'The Batch you selected does not exist'
                     : ' '
                 }
                 type={'number'}
               />
               <Button
-                sx={{ m: 1, marginTop: 3 }}
+                sx={{ marginLeft: '10px' }}
                 variant="contained"
                 onClick={() => {
                   createTransfers();
@@ -214,14 +227,17 @@ export const CreateCashBox: FC = () => {
               >
                 Transfar to CashBox
               </Button>
+              <br />
             </>
           )}
-          {transfers && (
+          {transfers && showSignTxBtn && (
             <Box sx={{ m: 1 }}>
-              <SignTransfersForm
-                transfers={transfers!}
-                cancel={() => setTransfers(undefined)}
-              />
+              <CashBoxContext.Provider value={{ setTxStatus }}>
+                <SignTransfersForm
+                  transfers={transfers!}
+                  cancel={() => setTransfers(undefined)}
+                />
+              </CashBoxContext.Provider>
             </Box>
           )}
         </div>
